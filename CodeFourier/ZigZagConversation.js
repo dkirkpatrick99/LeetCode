4bb60ef9009510daba40eab30b449be73c900ddd
CreatorDK <d.kirkpatrick99@gmail.com>
2020-05-12 14:03:31 -0700

    Leetcode

---
+++ CodeFourier/ZigZagConversation.js
@@ -0,0 +1,61 @@
+function convert (s, numRows) {
+    let rows = {}
+    let str = s.split('');
+    let i = 0;
+    let zig = 0;
+    let j = numRows - 2;
+    let k = 0;
+    let flag = false
+    while (k <= str.length - 1) {
+        
+        filling = i % numRows;
+        if (!rows[filling]) rows[filling] = []; 
+
+        if (flag) {
+            while(j > zig) {
+                if(!str[k]) break
+                for(let q = numRows - 1; q >= 0; q--) {
+                    if(q != j) {
+                        // rows[q].push(' ');
+                      
+
+                    } else {
+                        rows[q].push(str[k]);
+                        
+                        k++
+                        
+                    }
+                }
+                j--
+            }
+            i--
+            k--
+            flag  = false;
+
+        } else {
+
+            rows[filling].push(str[k])
+            
+            if (filling === numRows - 1) {
+                if(flag) {
+                    flag = false
+                } else {
+                    flag = true
+                }   
+            }
+
+        }
+
+        j = numRows - 2
+        k++
+        i++
+    }
+    let sent = ""
+    Object.values(rows).forEach((arr) => {
+        sent += arr.join('')
+    })
+    return sent
+
+};
+
+console.log(convert('PAYPALISHIRING', 5))
