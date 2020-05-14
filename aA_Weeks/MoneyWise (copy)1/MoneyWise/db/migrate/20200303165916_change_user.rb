68933ce4bafd9f85ff09a2ec47665d31bfec7e4a
CreatorDK <d.kirkpatrick99@gmail.com>
2020-05-13 21:09:17 -0700

    LeetCode

--- aA_Weeks/MoneyWise (copy)1/MoneyWise/db/migrate/20200303165916_change_user.rb
+++
@@ -1,5 +0,0 @@
-class ChangeUser < ActiveRecord::Migration[5.2]
-  def change
-    change_column :users, :username, :string
-  end
-end
--- aA_Weeks/aA_W8/Dalton_Kirkpatrick/app/models/poem.rb
+++
@@ -1,8 +0,0 @@
-class Poem < ApplicationRecord
-    validates :title, :stanzas, presence: true
-    validates :complete, inclusion: {in: [true, false]}
-
-    belongs_to :author,
-        foreign_key: :author_id,
-        class_name: :User
-end
